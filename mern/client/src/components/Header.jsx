928d958ccfa53a990e80db32dcd7cad9c5a2d615
noahbweiss <nbweiss6@gmail.com>
2025-04-02 13:48:27 -0700

    working header

--- mern/client/src/components/Header.jsx
+++ mern/client/src/components/Header.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 
-{
-/*
-
-this is a placeholder component, I wil flush this out later
-
-*/
-}
-const Header = () => {
+const Header = ({
+  currentPage
+}) => {
   return (
     <header className="bg-blue-500 text-white p-3 text-xl font-bold">
-      Blueprint
+      {currentPage}
     </header>
   );
 };
--- mern/client/src/components/MainLayout.jsx
+++ mern/client/src/components/MainLayout.jsx
@@ -1,39 +1,20 @@
-// import React from "react";
-// import Header from "./Header";
-// import CoursePlannerContainer from "./planner/CoursePlannerContainer";
-// import RightSidebar from "./right-sidebar/RightSidebar";
-
-// const MainLayout = () => {
-
-//   return (
-//     <div className="flex flex-col h-screen bg-gray-100">
-//       {/* Top Header */}
-
-//       {/* Main content area */}
-//       <div className="flex h-full w-full overflow-hidden">
-//         {/* Left: Planner (flexible space) */}
-//         <div className="flex-grow overflow-y-auto overflow-x-hidden">
-//           <CoursePlannerContainer />
-//         </div>
-
-//         {/* Right: Sidebar (fixed width controlled inside RightSidebar) */}
-//         <RightSidebar />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MainLayout;
-
 import React, { useState } from "react";
 import RightSidebar from "./right-sidebar/RightSidebar";
 import LeftSidebar from "./LeftSidebar";
 import CoursePlannerContainer from "./planner/CoursePlannerContainer"; // use existing planner
 import CourseStorage from "./CourseStorage";
 import QuarterlyView from "./QuarterlyView";
+import Header from "./Header";
 
 const MainLayout = () => {
+  
   const [currentPage, setCurrentPage] = useState("planner");
+
+  const pageTitles = {
+    planner: "4-Year Planner",
+    storage: "Course Storage",
+    quarter: "Quarter View",
+  };  
 
   const renderPage = () => {
     switch (currentPage) {
@@ -51,14 +32,23 @@ const MainLayout = () => {
   return (
     <div className="flex h-screen">
       <LeftSidebar setCurrentPage={setCurrentPage} currentPage={currentPage} />
-      
-      {/* Main content area */}
-      <div className="flex-grow p-6 overflow-y-auto">
-        {renderPage()}
-      </div>
 
-      {/* Right sidebar with course search & assistant */}
-      <RightSidebar />
+      {/* Main Panel: header + content + right sidebar */}
+      
+      <div className="flex flex-col flex-grow overflow-hidden">
+        <Header currentPage={pageTitles[currentPage] || "Blueprint"} />
+
+        <div className="flex flex-1 overflow-hidden">
+          {/* Main content area */}
+          <div className="flex-grow p-6 overflow-y-auto">
+            {renderPage()}
+          </div>
+
+          {/* Right sidebar with course search & assistant */}
+          <RightSidebar />
+
+        </div>
+      </div>
     </div>
   );
 };
